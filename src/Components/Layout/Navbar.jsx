a4f0af5cfdf7a663ee2429203b4aa8e6ac5c9f43
Jayant Kumar <jayantk237@gmail.com>
2022-11-11 19:00:50 +0530

    add navbar component

---
+++ src/Components/Layout/Navbar.jsx
@@ -0,0 +1,39 @@
+import { FaGithub } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+import React from "react";
+
+export default function Navbar({ title }) {
+  return (
+    <div className="navbar mb-12 shadow-lg bg-neutral text-neutral-content">
+      <div className="container mx-auto">
+        <div className="flex-none mx-2 px-2">
+          <FaGithub className="inline pr-2 text-3xl" />
+          <Link to="/" className="text-lg font-bold align-middle">
+            {title}
+          </Link>
+        </div>
+
+        <div className="flex-1 px-2 mx-2">
+          <div className="flex justify-end">
+            <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
+              Home
+            </Link>
+            <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
+              About
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+Navbar.defaultProps = {
+  title: "Github Finder",
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string,
+};
