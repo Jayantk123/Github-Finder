9211b5cdc8fac0d434b9c713526f26ef72bf114b
Jayant Kumar <jayantk237@gmail.com>
2022-11-17 19:14:00 +0530

    add user avatar and profile links

--- src/Components/users/UserResults.jsx
+++ src/Components/users/UserResults.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Spinner from "../Layout/Spinner";
+import UserItem from "./UserItem";
 
 export default function UserResults() {
   useEffect(() => {
@@ -26,7 +27,8 @@ if(!loading) {
     return (
         <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
           {users.map((user) => (
-            <h3>{user.login}</h3>
+            <UserItem key={user.id} user={user}/>
+            // <h3>{user.login}</h3>
           
           ))}
         </div>
