080a239790c0d6562958fbe7daaa748dbcf04164
Jayant Kumar <jayantk237@gmail.com>
2022-11-18 20:19:34 +0530

    add reducer & remove context

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,13 +1,21 @@
-import { createContext, useState } from "react";
-
+import { createContext, useReducer, useState } from "react";
+import githubReducer from "./GithubReducer";
 const GithubContext = createContext();
 
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 
 export const GithubProvider = ({ children }) => {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  // context
+  // const [users, setUsers] = useState([]);
+  // const [loading, setLoading] = useState(true);
 
+  // reducer
+
+  const initialState = {
+    users: [],
+    loading: true,
+  };
+  const [state, dispatch] = useReducer(githubReducer, initialState);
   const fetchUsers = async () => {
     const response = await fetch(`${GITHUB_URL}/users`, {
       // headers:{
@@ -18,15 +26,20 @@ export const GithubProvider = ({ children }) => {
 
     const data = await response.json();
 
-    setUsers(data);
-    setLoading(false);
+    // setUsers(data);
+    // setLoading(false);
+
+    dispatch({
+      type:'GET_USERS',
+      payload:data,
+    })
   };
 
   return (
     <GithubContext.Provider
       value={{
-        users,
-        loading,
+        users:state.users,
+        loading:state.loading,
         fetchUsers,
       }}
     >
