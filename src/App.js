ae024b5cf2abe9cf005292e5d94e1ad249c89f75
Jayant Kumar <jayantk237@gmail.com>
2022-12-05 20:19:24 +0530

    integrate alert component

--- src/App.js
+++ src/App.js
@@ -6,30 +6,30 @@ import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import { GithubProvider } from "./context/github/GithubContext";
 import { AlertProvider } from "./context/alert/AlertContext";
+import Alert from "./Components/Layout/Alert";
 function App() {
   return (
     <AlertProvider>
-    <GithubProvider>
-    <div>
-      <Router>
-        <div className="flex flex-col justify-between h-screen">
-          <Navbar />
+      <GithubProvider>
+        <div>
+          <Router>
+            <div className="flex flex-col justify-between h-screen">
+              <Navbar />
 
-          <main className="container mx-auto px-3 pb-12">
-<Routes>
-<Route path="/" element={<Home/>}/>
-<Route path="/about" element={<About/>}/>
-<Route path="/notfound" element={<NotFound/>}/>
-<Route path="/*" element={<NotFound/>}/>
-
-</Routes>
-
-          </main>
-          <Footer />
+              <main className="container mx-auto px-3 pb-12">
+                <Alert/>
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/about" element={<About />} />
+                  <Route path="/notfound" element={<NotFound />} />
+                  <Route path="/*" element={<NotFound />} />
+                </Routes>
+              </main>
+              <Footer />
+            </div>
+          </Router>
         </div>
-      </Router>
-    </div>
-    </GithubProvider>
+      </GithubProvider>
     </AlertProvider>
   );
 }
--- src/Components/users/UserSearch.jsx
+++ src/Components/users/UserSearch.jsx
@@ -1,14 +1,16 @@
 import React, { useState, useContext } from "react";
+import AlertContext from "../../context/alert/AlertContext";
 import GithubContext from "../../context/github/GithubContext";
 
 export default function UserSearch() {
   const [text, setText] = useState("");
-  const { users, searchUsers, clearSearch } = useContext(GithubContext);
+  const { users, searchUsers, clearSearch} = useContext(GithubContext);
+  const {setAlert}=useContext(AlertContext);
   const handleChange = (e) => setText(e.target.value);
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (text === "") {
-      alert("Please enter something");
+      setAlert('Please enter something','error');
     } else {
       searchUsers(text);
       setText(" ");
